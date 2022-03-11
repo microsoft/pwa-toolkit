284986cfdf0ff1e329bbe937940eaef6d97d3092
Derek Worthen <deworthe@microsoft.com>
2022-03-11 08:22:59 -0800

    fix api start script

--- examples/client/src/API.d.ts
+++ examples/client/src/API.d.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { NexusGenRootTypes } from 'api/dist/graphql/types'
+import { NexusGenRootTypes } from 'api/src/graphql/types'
 
 declare global {
   export namespace API {
